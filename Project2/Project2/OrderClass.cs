e4481df5eb1918cd35a187db60b0d86971745960
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2018-09-11 15:09:20 -0700

    Add files via upload

---
+++ Project2/Project2/OrderClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    // Created by Jacqueline Fonseca on 09/08/2018
+    class OrderClass
+    {
+        private int senderId, cardNo, amount, receiverID;
+        private double unit_price;
+
+        public int SenderId
+        {
+            get { return senderId; }
+            set { senderId = value;  }
+        }
+        public int CardNo
+        {
+            get { return cardNo; }
+            set { cardNo = value; }
+        }
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        public double Unit_Price
+        {
+            get { return unit_price; }
+            set { unit_price = value; }
+        }
+        public int ReceiverID
+        {
+            get { return receiverID; }
+            set { receiverID = value; }
+        }
+    }
+}
---
+++ Project2/Project2/Publisher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    // Created by Jacqueline Fonseca on 09/08/2018
+    class Publisher
+    {
+        private double price = 0; //price of books from pricemodel
+        private int p; // number of price cuts
+ 
+        public Publisher()
+        {
+            double newPrice = PricingModel;
+            if (newPrice < price)
+            {
+                //TODO: event handler to BOOKSTORE
+
+            }
+
+        }
+
+        public double PricingModel
+        {
+            // TODO: make a mathematical model to determine price
+            // we're using a random number generator for now
+            get
+            {
+                Random random = new Random();
+                int num = random.Next(50, 201);
+                return num;
+            }
+        }
+    }
+}
