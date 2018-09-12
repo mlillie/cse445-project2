b6d81d4ac10d8df61e134f60c6c9764a3955c2ba
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2018-09-11 23:50:22 -0700

    Update Publisher.cs

--- Project2/Project2/Publisher.cs
+++ Project2/Project2/Publisher.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,30 +9,30 @@ namespace Project2
     // Created by Jacqueline Fonseca on 09/08/2018
     class Publisher
     {
-        private double price = 0; //price of books from pricemodel
-        private int p; // number of price cuts
- 
-        public Publisher()
-        {
-            double newPrice = PricingModel;
-            if (newPrice < price)
-            {
-                //TODO: event handler to BOOKSTORE
+        Calendar c = new Calendar();
 
-            }
+        private double base_price = 0; //price of books from pricemodel
+        private double discount = 0;
+        private int p; // number of price cuts
+        private OrderClass order;
+        
+        public Publisher(string s)
+        {
+            base_price = (c.Base_Price(c.Day())).Item1;
+            discount = (c.Base_Price(c.Day()).Item2);
+
+            Cipher cipher = new Cipher();
+            order = cipher.decoder(s);
+
 
         }
 
-        public double PricingModel
+        public double PricingModel()
         {
-            // TODO: make a mathematical model to determine price
-            // we're using a random number generator for now
-            get
-            {
-                Random random = new Random();
-                int num = random.Next(50, 201);
-                return num;
-            }
+            double final_price = base_price * discount;
+            if ( order.Amount > 10 )
+                final_price = final_price - (order.Amount * .05);
+            return final_price; 
         }
     }
 }
