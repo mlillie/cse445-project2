52912b3636ee74e5909faaa86485e27e0dec88b7
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2018-09-14 16:19:56 -0700

    Update Publisher.cs

--- Project2/Project2/Publisher.cs
+++ Project2/Project2/Publisher.cs
@@ -12,26 +12,36 @@ namespace Project2
         Calendar c = new Calendar();
 
         private double base_price = 0; //price of books from pricemodel
-        private double discount = 0;
+        private double discount = 0; //discount to be used for that day
         private int p; // number of price cuts
-        private OrderClass order;
+        private double past_price = 0; 
+        private double new_price = 0;
         
-        public Publisher(string s)
+        public Publisher()
         {
             base_price = (c.Base_Price(c.Day())).Item1;
             discount = (c.Base_Price(c.Day()).Item2);
 
-            Cipher cipher = new Cipher();
-            order = cipher.decoder(s);
-
-
+            new_price = PricingModel(" ");
+            if (new_price < past_price)
+            {
+                //event
+                past_price = new_price;
+            }
+            else
+                past_price = new_price;
         }
 
-        public double PricingModel()
+        public double PricingModel(string s)
         {
+            Cipher cipher = new Cipher();
+            OrderClass order = cipher.decoder(s);
+
             double final_price = base_price * discount;
-            if ( order.Amount > 10 )
-                final_price = final_price - (order.Amount * .05);
+            if (order.Amount % 2 == 0 && order.Amount > 10)
+                final_price = final_price + (order.Amount * discount);
+            else
+                final_price = final_price - (order.Amount * discount);
             return final_price; 
         }
     }
