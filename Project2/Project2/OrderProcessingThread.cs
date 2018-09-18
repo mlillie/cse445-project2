c477c639eac3272c92f247d97699872c67bdb3f5
jfonsec3 <31457695+jfonsec3@users.noreply.github.com>
2018-09-17 19:16:52 -0700

    kool

---
+++ Project2/Project2/OrderProcessingThread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2
+{
+    class OrderProcessingThread
+    {
+        public static double tax;
+        public OrderProcessingThread()
+        {
+            tax = .083;
+        }
+        public static void OPT(OrderClass order)
+        {
+            if (order.CardNo < 1000 && order.CardNo > 100)
+            {
+                double total_amount = order.Unit_Price * order.Amount * tax;
+                Console.Write("Order: " + order.SenderId + "totaled $" + total_amount.ToString("0.00"));
+            }
+        }
+    }
+}
